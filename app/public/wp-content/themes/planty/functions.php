8c446e6e1d48bfea095e6cf581c83b6e91e8c241
s0led0b0ut <s0led0b0ut@gmail.com>
2024-08-05 11:32:09 +0200

    Initial commit

--- app/public/wp-content/themes/planty/functions.php
+++ app/public/wp-content/themes/planty/functions.php
@@ -21,32 +21,6 @@ function enqueue_custom_styles() {
 add_action( 'wp_enqueue_scripts', 'enqueue_custom_styles', 20 );
 
 
-/*function add_admin_menu_item($items, $args) {
-    // Check if the user is logged in and has the required capability
-    if (is_user_logged_in() && current_user_can('manage_options')) {
-        $items .= '<li><a href="' . admin_url() . '">Admin</a></li>';
-    }
-    return $items;
-}*/
-
-/*function add_admin_menu_item($items, $args) {
-    // Vérifier si l'utilisateur est connecté et a la capacité requise
-    if (is_user_logged_in() && current_user_can('manage_options')) {
-        $admin_item = '<li class="menu-item menu-item-admin"><a href="' . admin_url() . '">Admin</a></li>';
-        
-        // Diviser les éléments du menu en un tableau
-        $items_array = explode('</li>', $items);
-
-        // Insérer l'élément admin à la deuxième position
-        $position = 1; // Position 1 pour que l'élément apparaisse en deuxième position (après le premier élément)
-        array_splice($items_array, $position, 0, $admin_item);
-
-        // Rejoindre les éléments du menu en une chaîne
-        $items = implode('</li>', $items_array);
-    }
-    return $items;
-}
-*/
 function add_admin_menu_item($items, $args) {
     // Vérifier si l'utilisateur est connecté et a la capacité requise, et exclure le menu footer
     if (is_user_logged_in() && current_user_can('manage_options')) {
